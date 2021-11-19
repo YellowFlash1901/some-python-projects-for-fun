d807a11939ae3e35084a9de3df1c950844ce0b6c
Vardaan-sathe <65286264+YellowFlash1901@users.noreply.github.com>
2021-11-20 00:36:43 +0530

    added comments for better understanding

--- qrcode_genrator/qrcode_genrator.py
+++ qrcode_genrator/qrcode_genrator.py
@@ -2,12 +2,13 @@ import qrcode
 import image
 
 qr = qrcode.QRCode(
-    version = 9,
-    box_size = 10,
-    border = 5
+    version = 9, #add complexity to the qr higher the no. higher the complexity
+    box_size = 10, #it decides the no. of boxes in which the qrcode should be made
+    border = 5 #white part of image
+   
 )
 
-data = "https://github.com/YellowFlash1901"
+data = "https://github.com/YellowFlash1901" #add your own data to generate a qr code for your own data(website links,... etc).
 
 qr.add_data(data)
 qr.make(fit = True)
